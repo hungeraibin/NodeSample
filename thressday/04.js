3b6c7f773dccd9c475557072f9b281e3498a030b
aibin <1149299088@qq.com>
2018-07-15 22:43:16 +0800

    文件流、readfile

--- thressday/04.js
+++ thressday/04.js
@@ -1,9 +1,7 @@
-// 动态显示歌词
+// 动态显示歌词 流的方式读取
 
 const fs = require('fs');
 const path = require('path');
-const iconv = require('iconv-lite');
-const readline = require('readline');
 
 var filename = path.join(__dirname, './../lyrics/血染的风采.lrc')
 var streamReader = fs.createReadStream(filename);
@@ -17,23 +15,3 @@ streamReader.on('end', () =>{
   // 通知已经结束了此时data是完整的
   console.log(data);
 });
-
-var regex = /\[(\d{2})\:(\d{2})\.(\d{2})\]\s(.+)/;
-function task(line, begin) {
-  var matches = regex.exec(line);
-  if (matches) {
-    var m = parseFloat(matches[1]);
-    var s = parseFloat(matches[2]);
-    var f = parseFloat(matches[3]);
-    var lyric = matches[4];
-    // 由于下达输出任务的时刻不同
-    var offset = new Date().getTime() - begin;
-    setTimeout(() => {
-      console.log(lyric);
-    }, m*600*1000+s*1000+f-offset);
-
-  } else {
-    // 不是一行歌词
-    console.log(line)
-  }
-}
